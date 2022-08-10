beb84bb65061da496947978eafef095bb67c6457
Paywand Mohammed Hussein <73426000+paywand007@users.noreply.github.com>
2022-08-10 16:50:34 +0300

    complete assigmnment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+};
+
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(val) {
+    return function(value) {
+        return val * value;
+    };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, driversToReturn) {
+    return driversToReturn(drivers);
+}
